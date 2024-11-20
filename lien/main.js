810a324841c6bc00e5055efb8d683024e735682e
Santiago v <santiagovanegasoa@gmail.com>
2024-11-20 16:34:02 +0100

    update main page
    fichier main.js
    - nom  - username avec sessionstorage

---
+++ lien/main.js
@@ -0,0 +1,25 @@
+
+
+const telephone = sessionStorage.getItem("telephone");
+const prenom = sessionStorage.getItem("prenom");
+const email = sessionStorage.getItem("email");
+
+// Verificar valores importados
+console.log("Recovered values:", telephone, prenom, email);
+
+
+const myName=document.getElementById('prenom'); 
+
+myName.textContent = prenom;
+
+
+const username= document.getElementById('username'); 
+const usernamesplit = email.split('@')[0];
+
+username.textContent = `@${usernamesplit}` ;
+
+
+
+
+
+
